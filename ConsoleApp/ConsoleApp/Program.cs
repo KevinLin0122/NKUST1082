92fb5486c820a0a58110e3a094ca536dcc4cd0ae
YinChang <yinchang0626@gmail.com>
2019-03-05 11:55:14 +0800

    init

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace YC.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}

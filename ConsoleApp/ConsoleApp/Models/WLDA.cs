9ae7a3fa360600db4a71066d3129a5e6f57be71d
YinChang <yinchang0626@gmail.com>
2019-04-09 10:27:31 +0800

    add json waterdata

---
+++ ConsoleApp/ConsoleApp/Models/WLDA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YC.ConsoleApp.Models
+{
+    public class WaterLevelRealTime
+    {
+        public List<WaterLevelRecord> Value { get; set; }
+    }
+    public class WaterLevelRecord
+    {
+        public string StationIdentifier { get; set; }
+        public DateTime RecordTime { get; set; }
+        public double WaterLevel { get; set; }
+    }
+}
--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenDataImport.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Linq;
 
 namespace YC.ConsoleApp
 {
@@ -7,10 +15,115 @@ namespace YC.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
+            //var nodes = findOpenData();
+            //showOpenData(nodes);
+
+            var waterData = findWaterLevelRealTimeData();
+            showWaterData(waterData);
+            Console.ReadKey();
+
+        }
+        static List<OpenData> findOpenData()
+        {
+            List<OpenData> result = new List<OpenData>();
 
 
-            Console.WriteLine("Hello World!");
+
+            var xml = XElement.Load(@"App_Data/datagovtw_dataset_20181005.xml");
+
+
+            //XNamespace gml = @"http://www.opengis.net/gml/3.2";
+            //XNamespace twed = @"http://twed.wra.gov.tw/twedml/opendata";
+            List<XElement> nodes = xml.Descendants("node").ToList();
+
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+
+
+                OpenData item = new OpenData();
+
+                item.id = int.Parse(getValue(node, "id"));
+                item.資料集名稱 = getValue(node, "資料集名稱");
+                item.主要欄位說明 = getValue(node, "主要欄位說明");
+                item.服務分類 = getValue(node, "服務分類");
+                result.Add(item);
+            }
+
+            //result = nodes
+            //    .Where(x => !x.IsEmpty).ToList()
+            //    .Select(node =>
+            //    {
+            //        OpenData item = new OpenData();
+            //        item.ID = getValue(node, "ID");
+            //        item.資料集名稱 = getValue(node, "資料集名稱");
+            //        item.服務分類 = getValue(node, "服務分類");
+            //        item.資料集描述 = getValue(node, "資料集描述");
+            //        return item;
+
+            //    }).ToList();
+            return result;
+
+        }
+
+        static Models.WaterLevelRealTime findWaterLevelRealTimeData()
+        {
+            Models.WaterLevelRealTime result;
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string jsonStr = wc.DownloadString("http://odata.wra.gov.tw/v4/RealtimeWaterLevel?$top=1000");
+            result= JsonConvert.DeserializeObject<Models.WaterLevelRealTime>(jsonStr);
+
+            //JObject array = JsonConvert.DeserializeObject<JObject>(jsonStr);
+            //JsonSerializer serializer = new JsonSerializer();
+            //Models.WaterLevelRealTime data = (Models.WaterLevelRealTime)serializer.Deserialize(new JTokenReader(array), typeof(Models.WaterLevelRealTime));
+            return result;
+        }
+
+
+
+
+
+        private static string getValue(XElement node, string propertyName)
+        {
+            return node.Element(propertyName)?.Value?.Trim();
+
+        }
+
+
+        private static void showOpenData(List<OpenData> nodes)
+        {
+
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            //nodes.GroupBy(node => node.服務分類).ToList()
+            //    .ForEach(group =>
+            //    {
+
+            //        var key = group.Key;
+            //        var groupDatas = group.ToList();
+            //        var message = $"服務分類:{key},共有{groupDatas.Count()}筆資料";
+            //        Console.WriteLine(message);
+            //    });
+            nodes.ToList()
+                .ForEach(node =>
+                {
+                    var message = $"服務:{node.資料集名稱}";
+
+                    Console.WriteLine(message);
+                });
+            Console.WriteLine($"共 : {nodes.Count} 筆");
+
+        }
+
+        private static void showWaterData(Models.WaterLevelRealTime data)
+        {
+            data.Value.ForEach(item =>
+            {
+                Console.WriteLine($"Id:{item.StationIdentifier},Value:{item.WaterLevel}");
+
+
+            });
+
         }
     }
 
